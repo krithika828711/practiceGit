f3a0615725cf869ede423a94a5191bea7499070b
krithika828 <110586935+krithika828@users.noreply.github.com>
2023-05-19 15:43:49 +0530

    Modified Test1.java

--- src/githubpractice/Test1.java
+++ src/githubpractice/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n1 = 225, n2 = 115, sum;  
+		sum = n1+n2+1000;
 		sum = n1 + n2;  
 		System.out.println("The sum of numbers is: "+sum);  
 	}
